10c0a3c93e69ac74e6094a2d80ac31c9c5e18f56
Krishnanand V P <vpkrishnanand@gmail.com>
2022-08-04 18:01:43 +0530

    #1 : change to two pass hash table

--- 0001.rs
+++ 0001.rs
@@ -1,35 +1,24 @@
-use std::{collections::HashMap, ops::Sub, vec::Vec};
+// Leetcode #1: https://leetcode.com/problems/two-sum/
+//
+use std::{collections::HashMap, vec::Vec};
 struct Solution {}
 
 impl Solution {
-    pub fn solution(nums_c: Vec<i32>, _target: i32) -> Vec<i32> {
-        let mut nums = nums_c;
+    pub fn solution(nums: Vec<i32>, _target: i32) -> Vec<i32> {
         let mut num_map: HashMap<i32, i32> = HashMap::new();
 
         for (i, num) in nums.iter().enumerate() {
             num_map.insert(*num, i as i32);
         }
 
-        nums.sort();
-        let left: usize = 0;
-        let right: usize = nums.len().sub(1);
-        let result = Self::find(nums, _target, left, right);
-        let left_res = result[0];
-        let right_res = result[1];
-
-        return vec![num_map[left_res], num_map[right_res]];
-    }
-
-    fn find(nums: Vec<i32>, _target: i32, left: usize, right: usize) -> Vec<i32> {
-        let sum = nums[left as usize] + nums[right as usize];
-
-        if sum == _target {
-            return vec![nums[left as usize], nums[right as usize]];
-        } else if sum < _target {
-            return Self::find(nums, _target, left + 1, right);
+        for (i, num) in nums.iter().enumerate() {
+            let comp = _target - num;
+            if let Some(&index) = num_map.get(&comp) {
+                return vec![i as i32, index];
+            }
         }
 
-        return Self::find(nums, _target, left, right - 1);
+        vec![]
     }
 }
 
